4fd3502090a6e28599429a4e9d6f5f7fafe448d9
Akhilsai <codeakhilsai@gmail.com>
2026-06-24 20:03:21 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Pages/Home.jsx";
 import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
