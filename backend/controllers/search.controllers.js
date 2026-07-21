3730d775442da1517ed6178176eaaeddfe49b2e2
Akhilsai <codeakhilsai@gmail.com>
2026-07-21 19:53:09 +0530

    add multiple job parameters to cache

--- backend/controllers/search.controllers.js
+++ backend/controllers/search.controllers.js
@@ -9,9 +9,9 @@ const searchController = async (req, res) => {
   const jobLocation = location || "";
 
   const THREE_DAYS_IN_SECONDS = 3 * 24 * 60 * 60;
-
-  const cacheSearch = await client.get(query);
-  if (cacheSearch) return res.status(200).json(JSON.parse(cacheSearch));
+  const cacheKey = `search:${query}:${employmentTypes}:${jobLocation}`;
+  const cacheSearch = await client.get(cacheKey);
+  if (cacheSearch) return res.status(200).json(JSON.parse(cacheKey));
 
   const options = {
     method: "GET",
@@ -35,7 +35,7 @@ const searchController = async (req, res) => {
     const response = await axios.request(options);
 
     await client.setEx(
-      query,
+      cacheKey,
       THREE_DAYS_IN_SECONDS,
       JSON.stringify(response.data.data.jobs),
     );
