c0a5d06006063b4756a024947a8ae0c24e543553
Akhilsai <codeakhilsai@gmail.com>
2026-07-30 20:27:35 +0530

    .

--- backend/controllers/Registergooglecontroller.js
+++ backend/controllers/Registergooglecontroller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.models.js";
 import jwt from "jsonwebtoken";
-import { sendVerificationEmail } from "./verifyEmailController.js";
+import { sendVerificationEmail } from "../controllers/verifyemail.controllers.js";
 
 const registerGoogleController = async (req, res) => {
   const { email, username, firebaseUid } = req.body;
