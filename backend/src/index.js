e4f7e5a14b3c609a125c19f530b7d096489dcf4d
Akhilsai <codeakhilsai@gmail.com>
2026-07-30 21:35:23 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
